a83cac3583d38ad12bacb239d8d90617505c8073
jvd33 <jvd5839@rit.edu>
2020-02-05 05:59:31 +0000

    Updated UI for release

--- tps-calc-ui/src/components/About.vue
+++ tps-calc-ui/src/components/About.vue
@@ -7,12 +7,10 @@
           <br/>
           or send gold to Aemin - Skeram US (pls)
           <br/><br/>
-          There's a public API (barely an API), documentation can be viewed at
+          There's a public API, documentation can be viewed at
           <a class="text-accent text-weight-bold" href="/api/v1/docs">
             https://classicthreat.com/api/v1/docs
-          </a>. There's almost no functionality, but the website is driven by an accessible REST API.
-          <br/><br/>
-          Don't break it, I have done 0 load testing
+          </a>. There's very little functionality, but the website is driven by an accessible REST API.
           <br/><br/>
           Feature Requests and Bug Reports can be submitted on GitHub:
           <a class="text-accent text-weight-bold" href="https://github.com/jvd33/ClassicTPSEstimator/issues">
@@ -25,9 +23,10 @@
           <br/><br/>
           <span class="text-primary text-h4">Future planned features:<br/></span><br/>
             - Split TPS across individual mobs in the boss encounters (e.g. Lucifron TPS, Flamewaker 1 TPS, Flamewaker 2 TPS) <br/>
-            - BEAR DRUIDS, more set bonuses/modifiers of various kinds, various spell ranks <br/>
-            - Sequential fight event parsing <br/>
-            - Literally any other valuable analysis because this is so much easier than digging through the WarcraftLogs UI. If you've got any ideas, submit a feature request<br/><br/>
+            - More set bonuses/modifiers of various kinds, more spell ranks <br/>
+            - TPS Graphing (Coming Soon!)<br/>
+            - Comparing against other players previously submitted to ClassicThreat <br/>
+            - Non-tank roles
           <span class="text-primary text-h6">Thanks to:<br/></span>
             - Dhoris/AutumnRain for his great work on the TPS spreadsheet that inspired this<br/>
             - Fight Club/magey's classic-warrior GitHub/libthreat2/everyone who has contributed to our understanding of threat mechanics <br/>
